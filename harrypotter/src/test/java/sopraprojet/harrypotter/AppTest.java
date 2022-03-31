4e524021966db4f6261585024fa62be6d84e3e99
IsmailBenosman <benosman.ismail.ups@gmail.com>
2022-03-31 15:55:47 +0200

    +test

---
+++ harrypotter/src/test/java/sopraprojet/harrypotter/AppTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import compte.Eleve;
+import config.AppConfig;
+import service.EleveService;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { AppConfig.class })
+class AppTest {
+
+	@Autowired
+	EleveService eleveService;
+	
+	@Disabled
+	@Test
+	@Transactional
+	@Commit
+	void CreationEleveTest() {
+		Eleve e1 = new Eleve("Pierson", "Robin", "rob", "rob", LocalDate.parse("1997-03-18"), 0,"Serpentard");
+		Eleve e2 = new Eleve("Vong", "Michel", "michel", "michel", LocalDate.parse("1992-03-18"), 0,"Poufsouffle");
+		eleveService.create(e1);
+		eleveService.create(e2);
+	}
+
+	@Test
+	void eleveservicetest() {
+		assertNotNull(eleveService);
+	}
+}
