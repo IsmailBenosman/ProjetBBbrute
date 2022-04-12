33d0876bb51a6cf3541e15181b54d8b0683b3fc6
Elisabeth <elisabeth.lisha.ye@gmail.com>
2022-04-12 18:19:21 +0200

    Livraison

--- harrypotter/src/main/java/sopraprojet/harrypotter/repositories/LivraisonRepository.java
+++ harrypotter/src/main/java/sopraprojet/harrypotter/repositories/LivraisonRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface LivraisonRepository extends JpaRepository<Livraison, Integer>{
-    Livraison getById(Integer id);
+   
 }
--- harrypotter/src/main/java/sopraprojet/harrypotter/service/LivraisonService.java
+++ harrypotter/src/main/java/sopraprojet/harrypotter/service/LivraisonService.java
@@ -1,7 +1,12 @@
 package sopraprojet.harrypotter.service;
 
+
 import sopraprojet.harrypotter.boutique.Livraison;
 import sopraprojet.harrypotter.repositories.LivraisonRepository;
+import sopraprojet.harrypotter.repositories.LivraisonRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,16 +16,15 @@ public class LivraisonService {
 	@Autowired
 	private LivraisonRepository livraisonRepository;
 
-	public void create(Livraison livraison) {
-		//TODO: creates new data without checking for duplicates
-		livraisonRepository.save(livraison);
-	}
 
-	public void delete(Integer id) {
-		livraisonRepository.delete(getById(id));
+	public List<Livraison> getAll() {
+		return livraisonRepository.findAll();
 	}
-
+	
 	public Livraison getById(Integer id) {
-		return livraisonRepository.findById(id).orElseThrow(RuntimeException::new);
+		return livraisonRepository.getById(id);
 	}
+
+
+
 }
