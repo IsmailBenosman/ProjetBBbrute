7109d03e792888378059b3c15845ec5aa549dfe4
MatthieuPinel <mattpinel23.mp@gmail.com>
2022-04-13 09:10:32 +0200

    ajout methode cours en fonction de eleve

--- harrypotter/src/main/java/sopraprojet/harrypotter/repositories/CoursRepository.java
+++ harrypotter/src/main/java/sopraprojet/harrypotter/repositories/CoursRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import sopraprojet.harrypotter.compte.Eleve;
 import sopraprojet.harrypotter.compte.Prof;
 import sopraprojet.harrypotter.ecole.Cours;
 
@@ -15,6 +16,8 @@ public interface CoursRepository extends JpaRepository<Cours, Integer>{
 	void deleteByProf(@Param("prof") Prof prof);*/
 	List<Cours> findByProfesseur(Prof p);
 	
-	/*@Query("select p from cours where prof = ?1 ")
+	List<Cours> findByEleve(Eleve eleve);
+	
+	/*@Query("select c from Cours cours where  = ?1 ")
 	public List<Cours> findByprof(Integer id); */
 }
